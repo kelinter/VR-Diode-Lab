9565134522942555b54a11535e1a28dc02039b18
Kelly Interiano <kki001@bucknell.edu>
2026-04-15 22:25:00 -0400

    Add snap-back behavior and audio reset for 3 interactable components

--- Assets/MyScripts/PlayAudioOnGrab.cs
+++ Assets/MyScripts/PlayAudioOnGrab.cs
@@ -1,24 +1,64 @@
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
 public class PlayAudioOnGrab : MonoBehaviour
 {
     private AudioSource audioSource;
-    private UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable grabInteractable;
+    private XRGrabInteractable grabInteractable;
+    private Rigidbody rb;
+    private FloatRotate floatRotate;
+
+    [SerializeField] private Transform homePoint;
 
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
-        grabInteractable = GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable>();
+        grabInteractable = GetComponent<XRGrabInteractable>();
+        rb = GetComponent<Rigidbody>();
+        floatRotate = GetComponent<FloatRotate>();
 
         grabInteractable.selectEntered.AddListener(OnGrab);
+        grabInteractable.selectExited.AddListener(OnRelease);
     }
 
     void OnGrab(SelectEnterEventArgs args)
     {
-        if (audioSource != null && !audioSource.isPlaying)
+        if (audioSource != null)
         {
+            audioSource.Stop();
             audioSource.Play();
+        }
+
+        if (floatRotate != null)
+        {
+            floatRotate.enabled = false;
+        }
+    }
+
+    void OnRelease(SelectExitEventArgs args)
+    {
+        if (audioSource != null)
+        {
+            audioSource.Stop();
+        }
+
+        if (rb != null)
+        {
+            rb.linearVelocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
+
+        if (homePoint != null)
+        {
+            transform.position = homePoint.position;
+            transform.rotation = homePoint.rotation;
+        }
+
+        if (floatRotate != null)
+        {
+            floatRotate.ResetFloatOrigin();
+            floatRotate.enabled = true;
         }
     }
 }
